277cdd6788e6d52213752543d0d849e86ea3cc74
David Fernandez <davofg@gmail.com>
2025-06-15 22:50:15 +0200

    Implement GetStatus with QueryHandler

--- src/health-check/domain/app-status.ts
+++ src/health-check/domain/app-status.ts
@@ -1,10 +1,18 @@
 export class AppStatus {
   constructor(
-    public readonly status: 'ok',
-    public readonly timestamp: string
+    private readonly _status: 'ok',
+    private readonly _timestamp: string
   ) {}
 
   static createOk(): AppStatus {
     return new AppStatus('ok', new Date().toISOString());
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+  get timestamp(): string {
+    return this._timestamp
   }
 }
--- src/health-check/health-check.module.ts
+++ src/health-check/health-check.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { HealthCheckController } from './infrastructure/controller/health-check.controller';
+import { GetAppStatusQueryHandler } from './application/get-app-status/get-app-status.query-handler';
+import { AppStatusGetter } from './domain/service/app-status-getter';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [HealthCheckController],
+  providers: [GetAppStatusQueryHandler, AppStatusGetter],
 })
-export class HealthCheckModule {}
+export class HealthCheckModule { }
