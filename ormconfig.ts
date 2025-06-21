8bf5d977e036582b549ee69764987b1583c142eb
David Fernández <davofg@users.noreply.github.com>
2025-06-21 12:19:37 +0200

    Feat: ORM set up (#3)
    * ORM set up

---
+++ ormconfig.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD ,
+  database: process.env.POSTGRES_DB ,
+  entities: ['src/**/infrastructure/persistence/typeorm/*.entity.ts'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false, // never use true in production
+});
