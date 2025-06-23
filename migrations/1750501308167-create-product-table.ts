a65c8f2be4eac6bed9fc76da59bf852c56372eda
David Fernandez <davofg@gmail.com>
2025-06-23 09:16:26 +0200

    Create product table migration

--- migrations/1750501308167-create-product-table.ts
+++ migrations/1750501308167-create-product-table.ts
@@ -4,11 +4,11 @@ export class CreateProductTable1750501308167 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-        CREATE TABLE product (
-            id UUID PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            price NUMERIC(10, 2) NOT NULL
-        )
+            CREATE TABLE product (
+                id UUID PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                price NUMERIC(10, 2) NOT NULL
+            )
         `);
     }
 
