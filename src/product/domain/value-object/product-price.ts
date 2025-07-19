de971a9d014bebf08d574b1cadb8d7051a30a479
David Fernandez <davofg@gmail.com>
2025-07-19 12:19:34 +0200

    Mapping and migration

--- src/product/domain/value-object/product-price.ts
+++ src/product/domain/value-object/product-price.ts
@@ -1,5 +1,7 @@
 export class ProductPrice {
-  public constructor(private readonly _value: number) {
+  public constructor(
+    private readonly _value: number
+  ) {
     if (isNaN(_value) || _value < 0) {
       throw new Error('Product price must be a non-negative number.');
     }
--- src/product/infrastructure/typeorm/typeorm-product.entity.ts
+++ src/product/infrastructure/typeorm/typeorm-product.entity.ts
@@ -5,7 +5,7 @@ export class TypeOrmProduct {
     @PrimaryColumn('uuid')
     id: string;
 
-    @Column()
+    @Column({ length: 150 })
     name: string;
 
     @Column('decimal', { precision: 14, scale: 2 })
