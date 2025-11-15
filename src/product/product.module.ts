a74f9ea5faffc29772e5bb8e3a6e4ede4b2e12ad
David Fernandez <davofg@gmail.com>
2025-11-15 14:52:15 +0100

    Updated product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { GetProductController } from './infrastructure/controller/product.controller';
+import { ProductController } from './infrastructure/controller/product.controller';
 import { GetProductQueryHandler } from './application/get/get-product.query-handler';
 import { TypeOrmProductRepository } from './infrastructure/typeorm/typeorm-product.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmProduct } from './infrastructure/typeorm/typeorm-product.entity';
+import { UpdateProductPriceCommandHandler } from './application/update-price/update-product-price.command-handler';
 
 @Module({
   imports: [
@@ -12,10 +13,15 @@ import { TypeOrmProduct } from './infrastructure/typeorm/typeorm-product.entity'
     TypeOrmModule.forFeature([TypeOrmProduct])
   ],
   controllers: [
-    GetProductController
+    ProductController
   ],
   providers: [
     GetProductQueryHandler, 
+    {
+      provide: 'ProductRepository',
+      useClass: TypeOrmProductRepository
+    },
+    UpdateProductPriceCommandHandler, 
     {
       provide: 'ProductRepository',
       useClass: TypeOrmProductRepository
